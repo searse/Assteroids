38fd9e3407b2fe96cf2a4a59c56199ee7f76d46f
searse <searse@gmail.com>
2025-09-29 18:30:32 -0700

    60 fps

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ import pygame
 from constants import *
 
 def main():
+    print("Starting Asteroids!")
+    print("Screen width: 1280")
+    print("Screen height: 720")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
+        screen.fill((0,0,0))
         pygame.display.flip()
-
-    print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
