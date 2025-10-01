d7ffcc435e07d19f1bc1cc632406429f1ead1855
searse <searse@gmail.com>
2025-09-30 18:44:07 -0700

    player rotation

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def main():
     # init pygame
     pygame.init()
 
-    # declare screen as instance of pygame.display with set_mode() method to define screen dimensions
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     # declare clock as instance of pygame.time.Clock()
     clock = pygame.time.Clock()
 
     # declare dt
     dt = 0
+
+    # declare screen as instance of pygame.display with set_mode() method to define screen dimensions
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # declare Player as instance of Player(CircleShape)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
@@ -30,12 +30,13 @@ def main():
     # Game Loop
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: #listen for quit in pygame window
                 return
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000 #limit the FPS to 60
+        player.update(dt) # update the player rotation status
+        screen.fill("black") # fill the screen
+        player.draw(screen) # draw the player on the screen
+        pygame.display.flip() #recycle the screen
 
 # Main function protection
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -2,7 +2,7 @@
 import pygame 
 
 # imports
-from constants import PLAYER_RADIUS
+from constants import *
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -21,3 +21,14 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.circle(screen, "red", self.position, self.radius, 2)
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+                self.rotate(-dt)
+        if keys[pygame.K_d]:
+                self.rotate(dt)
